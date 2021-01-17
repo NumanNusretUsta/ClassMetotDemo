cd145087851d43baca195ac6c3fd577e8f0c238f
Numan Nusret Usta <numanusta75@gmail.com>
2021-01-18 00:09:49 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi "+musteri.Name + " "+musteri.LastName);
+        }
+        public void Remove(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi " + musteri.Name + " " + musteri.LastName);
+        }
+        public void List(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Id+" "+musteri.Name+" "+musteri.LastName+" "+musteri.Rating);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Name = "Engin";
+            musteri1.LastName = "Demiroğ";
+            musteri1.Rating = 99;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+
+            Musteri[] musteriler = new Musteri[] {musteri1 };
+            musteriManager.List(musteriler);
+
+        }
+    }
+}
